887224aef6b68a5880177e52013bd73296a095a1
eduerjb <51493922+eduerjb@users.noreply.github.com>
2020-04-01 11:39:47 -0500

    eduer
    cambio prueba

--- misitioguia.php
+++ misitioguia.php
@@ -225,6 +225,7 @@ if (isset($_SESSION['guia'])) {
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
+<!-- hola -->
 </html>
 
 <?php
