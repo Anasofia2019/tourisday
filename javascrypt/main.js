bb102f0d70196d42900dbed0ed86b9472dbf15e5
eduerjb <51493922+eduerjb@users.noreply.github.com>
2020-04-20 09:40:46 -0500

    Update main.js
    documentacion de codigo javascrypt

--- javascrypt/main.js
+++ javascrypt/main.js
@@ -1,8 +1,11 @@
+// se crea el evento de categorización de los tabs y de los articulos al cargar la pagina
 $(document).ready(function(){
 	$('ul.tabs li a:first').addClass('active');
 $('.secciones article').hide();
 $('.secciones article:first').show();
 
+
+// aqui hacemos uso de los efectos de jquery como hide o show para esconder o prolongar los articulos
 $('ul.tabs li a').click(function(){
  $('ul.tabs li a').removeClass('active');
  $(this).addClass('active');
