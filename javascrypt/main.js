fa7bf7eb874c5676c62f4a9328172ceb5902cde6
eduerjb <51493922+eduerjb@users.noreply.github.com>
2020-06-07 16:59:48 -0500

    eduer jaramillo
    correción documentación javacrypt

--- javascrypt/main.js
+++ javascrypt/main.js
@@ -1,20 +1,56 @@
-// se crea el evento de categorización de los tabs y de los articulos al cargar la pagina
-$(document).ready(function(){
+/* Al cargar las páginas,misitioguia.php
+ o misitioturista.php,
+ se ejecutará la función
+ que activa el color amarillo
+en el primer botón que se encuentra en la navbar,
+el valor active equivale a el color amarillo
+definido en las hojas de estilo misitioguia.css y
+misitioturista.css
+ */
+ $(document).ready(function(){
 	$('ul.tabs li a:first').addClass('active');
 $('.secciones article').hide();
 $('.secciones article:first').show();
 
 
-// aqui hacemos uso de los efectos de jquery como hide o show para esconder o prolongar los articulos
+/*
+la siguiente función
+lo que nos permite es
+crear un evento, al dar
+click en uno de los botones
+que se encuentra en la navbar,
+ese botón obtiene un tono amarillo
+y los demás botones tendrán el
+color original.
+
+*/
+
 $('ul.tabs li a').click(function(){
  $('ul.tabs li a').removeClass('active');
  $(this).addClass('active');
+
+/*en este caso el efecto
+hide de jquery, lo que nos permite
+es esconder las etiquetas article
+que se  encuentran dentro de las etiquetas
+seccion cuando el tab que las contiene no
+está activado */
+
  $('.secciones article').hide();
 
-// aqui  nuestro javascrypt va a realizar un cambio de articulo por el tarjeteo de los href
-// lo que significa que buscara todos los valores coincidentes
+
+/* en caso de que se active un tab
+cualquiera, es decir cada que demos
+clik en un boton de la nav,el articulo
+mostrará la información coincidente
+que direccione el atributo href de la
+etiqueta article*/
+
+
  var activeTab = $(this).attr('href');
  $(activeTab).show();
+/* en caso de de no coincidir el
+ metodo attr arroja false y no ejecuta nada */
  return false;
 });
 
